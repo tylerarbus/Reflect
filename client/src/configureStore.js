cf6b6622ffa0ee03e8f10fed68f05aabd171d095
Tyler Arbus <tyler.arbus@gmail.com>
2017-04-08 13:31:03 -0700

    Add HMR and eslint

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -10,12 +10,20 @@ const loggerMiddleware = createLogger();
 const history = createHistory();
 const router = routerMiddleware(history);
 
-const configureStore = function (preloadedState) {
-  return createStore(
+const configureStore = preloadedState => {
+  const store = createStore(
     rootReducer,
     preloadedState,
     applyMiddleware(thunkMiddleware, loggerMiddleware, router)
-  )
-}
+  );
 
+  if(module.hot) {
+    module.hot.accept('./reducers/reducers.js', () => {
+      const nextReducer = require('./reducers/reducers.js').default;
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
+}
 export { history, configureStore }
