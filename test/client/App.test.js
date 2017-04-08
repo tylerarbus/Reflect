fb84daa949a69d366407cbfcf22c3c7f99a03947
Scott <smclendening@gmail.com>
2017-04-08 13:59:42 -0700

    Add more component tests

--- test/client/App.test.js
+++ test/client/App.test.js
@@ -3,7 +3,22 @@ import { shallow, mount, render } from 'enzyme';
 
 import { App } from '../../client/src/components/App.jsx';
 
-it('should render without crashing', () => {
-  const wrapper = shallow(<App />);
-});
+describe('App Component', () => {
+  let app;
+
+  beforeEach(() => {
+    app = shallow(<App />);
+  })
+
+  it('should render without crashing', () => {
+    const wrapper = app;
+  });
+
+  it('App renders nested components', () => {
+    expect(app.find('Entries').length).toEqual(1);
+  })
+
+})
+
+
 
---
+++ test/client/Entry.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import Entry from '../../client/src/components/Entry.jsx';
+
+describe('Entry Component', () => {
+  let entry;
+
+  beforeEach(() => {
+    entry = mount(<Entry date={'Jan 1, 2017'} text={'I had a good day.'}/>);
+  })
+
+  it('should render without crashing', () => {
+    const wrapper = entry;
+  });
+
+  it('Entry renders a div', () => {
+    expect(entry.find('div').length).toEqual(1);
+  });
+
+  it('Entry requires date and text props', () => {
+    expect(entry.props().date).toBeDefined();
+    expect(entry.props().text).toBeDefined();
+  })
+  
+})
+
