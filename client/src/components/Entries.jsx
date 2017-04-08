58f8fe778d61aaf3dee0cd0d67c918824b5abf89
Scott <smclendening@gmail.com>
2017-04-07 22:51:26 -0700

    Add Entries component

---
+++ client/src/components/Entries.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default class Entries extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Entries Component</div>
+    )
+  }
+}
