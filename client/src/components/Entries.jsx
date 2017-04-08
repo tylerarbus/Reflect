e9ea2ad539220a2c26b5e8c1bfe11e3bcbb16c05
Scott <smclendening@gmail.com>
2017-04-08 15:07:25 -0700

    Edit dummy data

--- client/src/components/Entries.jsx
+++ client/src/components/Entries.jsx
@@ -5,12 +5,11 @@ import Entry from './Entry.jsx';
 const dummyEntries = [
   {
     date: 'Jan 3, 2017', 
-    text: 'Today was a good day. I counted \
-    my money for a few hours and then ate some expensive snails. I love money!'
+    text: 'Test entry #1'
   },
   {
     date: 'April 6, 2017',
-    text: 'I got a few more people sick today. Awesome! Greed is good baby!'
+    text: 'Test entry #2'
   }
 ];
 
