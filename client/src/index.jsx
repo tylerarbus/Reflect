f8f67447eb47b0a803813a4833e977128ef062c7
Jason Chang <jay3c14@gmail.com>
2017-04-07 20:46:27 -0700

    Add route to /

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 import { Provider } from 'react-redux';
-import configureStore from './configureStore.js';
+import { history, configureStore } from './configureStore.js';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter, push } from 'react-router-redux';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact path="/" component={App}/>
+      </div>
+    </ConnectedRouter>
   </Provider>, document.getElementById('app')
 );
