6cdb77570e164cb32c0d4cba32ce25a13b4ebff0
Jason Chang <jay3c14@gmail.com>
2017-04-08 16:36:14 -0700

    Add Nav component test cases

--- test/client/Nav.test.js
+++ test/client/Nav.test.js
@@ -3,6 +3,22 @@ import { shallow, mount, render } from 'enzyme';
 
 import { Nav } from '../../client/src/components/Nav.jsx';
 
-it('should render without crashing', () => {
-  const wrapper = shallow(<Nav />);
+describe('Nav Component', () => {
+
+  let navbar;
+
+  beforeEach(() => {
+    navbar = shallow(<Nav />);
+  });
+
+  it('should render without crashing', () => {
+  });
+
+  it('should render a div with id "navbar"', () => {
+    expect(navbar.find('#navbar').length).toEqual(1);
+  });
+
 });
+
+
+
