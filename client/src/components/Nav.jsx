2187aeaa5dd03e33f6a74cac3ee74fe079f9d4e3
Jason Chang <jay3c14@gmail.com>
2017-04-08 16:44:53 -0700

    Create Nav component with links to / and /entries

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 
 export class Nav extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+
+    this.onClickHome = this.onClickHome.bind(this);
+    this.onClickEntries = this.onClickEntries.bind(this);
+  }
+
+  onClickHome() {
+    this.props.dispatch(push('/'));
+  }
+
+  onClickEntries() {
+    this.props.dispatch(push('/entries'));
   }
 
   render() {
     return(
-      <div id="navbar">
+      <div id="navbar" className="ui menu">
+        <div className="ui container">
+          <a onClick={this.onClickHome} className="item">Reflective</a>
+          <a onClick={this.onClickEntries} className="item">Entries</a>
+          <div className="right menu">
+            <a className="item">Profile</a>
+          </div>
+        </div>
       </div>
     )
   }
