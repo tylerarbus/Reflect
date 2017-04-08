a89ffcf5004424d4d68636d107348a5d73118645
Scott <smclendening@gmail.com>
2017-04-07 22:40:41 -0700

    Write baseline test for App component

---
+++ test/client/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+import { App } from '../../client/src/components/App.jsx';
+
+it('should render without crashing', () => {
+  const wrapper = shallow(<App />);
+});
