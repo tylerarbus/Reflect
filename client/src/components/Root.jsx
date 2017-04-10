515745c187f74bb3597e13be9dbd1748a00b943e
Tyler Arbus <tyler.arbus@gmail.com>
2017-04-09 22:21:11 -0700

    (bug) Fix hot module reloader

---
+++ client/src/components/Root.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux';
+import { Route } from 'react-router-dom';
+
+import App from './App.jsx';
+import Nav from './components/Nav.jsx';
+
+export default class Root extends React.Component {
+  render() {
+    return (
+      <Provider store={this.props.store} >
+        <Nav>
+          <ConnectedRouter history={this.props.history}>
+            <Route path='/' component={App} />
+          </ConnectedRouter>
+        </Nav>
+      </Provider>
+    )
+  }
+}
