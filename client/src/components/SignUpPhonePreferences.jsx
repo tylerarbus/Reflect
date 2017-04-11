50e3b9663e7861aab12909c5991b401b81f9960b
Jason Chang <jay3c14@gmail.com>
2017-04-11 16:02:08 -0700

    Add SignUpPhonePreferences component to render step 3 of signup

---
+++ client/src/components/SignUpPhonePreferences.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export class SignUpPhonePreferences extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <form className="ui form">
+        <h4 className="ui dividing header">Call Preferences</h4>
+        <div className="field">
+          <label>Scheduled Call Time</label>
+          <div className=" three fields">
+            <div className="two wide field">
+              <select className="ui fluid search dropdown">
+                <option value="">Hour</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+              </select>
+            </div>
+            :
+            <div className="two wide field">
+              <select className="ui fluid search dropdown">
+                <option value="">Minute</option>
+                <option value="00">00</option>
+                <option value="05">05</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+                <option value="20">20</option>
+                <option value="25">25</option>
+                <option value="30">30</option>
+                <option value="35">35</option>
+                <option value="40">40</option>
+                <option value="45">45</option>
+                <option value="50">50</option>
+                <option value="55">55</option>
+              </select>
+            </div>
+            <div className="two wide field">
+              <select className="ui fluid search dropdown">
+                <option value="">Select</option>
+                <option value="AM">AM</option>
+                <option value="PM">PM</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </form>
+    )
+  }
+
+}
+
+const mapStateToProps = (state) => {
+  return {
+    ...state
+  }
+}
+
+export default connect(mapStateToProps)(SignUpPhonePreferences);
