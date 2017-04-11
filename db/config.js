252e1f93a6b95035902cc89ccbf715db755b85f9
Tyler Arbus <tyler.arbus@gmail.com>
2017-04-11 11:05:29 -0700

    Changes database url to heroku for deployment testing

--- db/config.js
+++ db/config.js
@@ -1,11 +1,11 @@
 const pgp = require('pg-promise')();
 const schema = require('./schema.js');
 
-const url = process.env.DATABASE_URL || 'postgres://@localhost:5432/reflective'
+const url = 'postgres://fxfnmczpnmrlby:c73f1ad4380825fbda4e6e23a315bb248b718c42cb4449ba2c1714430264980d@ec2-23-23-225-12.compute-1.amazonaws.com:5432/dbtu8fljhml504'
 
-if (process.env.DATABASE_URL) {
+//if (process.env.DATABASE_URL) {
   pgp.pg.defaults.ssl = true;
-};
+//};
 
 const db = pgp(url);
 
