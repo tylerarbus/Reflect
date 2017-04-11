92ed178b3b2fdc6d127e09e6f31209347136a7c2
Jason Chang <jay3c14@gmail.com>
2017-04-11 11:38:46 -0700

    Add routes to /, /entries, and /signup to router and add Nav component

--- client/src/components/Root.jsx
+++ client/src/components/Root.jsx
@@ -4,15 +4,24 @@ import { ConnectedRouter } from 'react-router-redux';
 import { Route } from 'react-router-dom';
 
 import App from './App.jsx';
+import Home from './Home.jsx';
 import Nav from './Nav.jsx';
+import SignUp from './SignUp.jsx';
 
 export default class Root extends React.Component {
   render() {
     return (
       <Provider store={this.props.store} >
-        <ConnectedRouter history={this.props.history}>
-          <Route exact path='/' component={App} />
-        </ConnectedRouter>
+        <div>
+          <Nav/>
+          <ConnectedRouter history={this.props.history}>
+            <div>
+              <Route exact path='/' component={Home} />
+              <Route path='/entries' component={App} />
+              <Route path='/signup' component={SignUp} />
+            </div>
+          </ConnectedRouter>
+        </div>
       </Provider>
     )
   }
