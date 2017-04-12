9e08e60d68b01b200d82ed9935f1c05efb26eb48
Jason Chang <jay3c14@gmail.com>
2017-04-11 18:51:48 -0700

    Add onChange to email field which updates email in store

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+import { userSubmitEmail } from '../actions/actions.js';
 
 export class Home extends Component {
   constructor(props) {
@@ -8,10 +9,15 @@ export class Home extends Component {
     this.state = {};
 
     this.onSubmitEmail = this.onSubmitEmail.bind(this);
+    this.onEmailFieldChange = this.onEmailFieldChange.bind(this);
   }
 
   onSubmitEmail() {
     this.props.dispatch(push('/signup'));
+  }
+
+  onEmailFieldChange(email) {
+    this.props.dispatch(userSubmitEmail(email));
   }
 
   render() {
@@ -21,11 +27,12 @@ export class Home extends Component {
           <h1 className="ui inverted header">Reflective</h1>
           <form className="ui form">
             <div className="field">
-              <input type="text" placeholder="E-mail address"></input>
+              <input type="text" placeholder="E-mail address"
+                     onChange={(e) => {this.onEmailFieldChange(e.target.value)}}></input>
             </div>
             <div className="ui fluid large teal submit button"
                  onClick={this.onSubmitEmail}>
-              Create an Account
+              Get Started
             </div>
           </form>
         </div>
