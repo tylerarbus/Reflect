5b39b6b49ca5a4e4ff07f3fd66a296be0dfbd4ad
Scott <smclendening@gmail.com>
2017-04-12 22:57:45 -0700

    Add entry-text insert function

---
+++ server/models/entry-text.js
@@ -0,0 +1,10 @@
+const { db } = require('../../db/config.js');
+
+module.exports.insert = (entry_id, text) => {
+  return db.one(
+    'INSERT INTO entry_text\
+    (entry_id, text)\
+    VALUES ($1, $2)\
+    RETURNING entry_text_id',
+    [entry_id, text])
+}
