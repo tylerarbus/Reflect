097b6556cb77385499f3c98d7cb9a27b04619b67
Scott <smclendening@gmail.com>
2017-04-13 21:34:36 -0700

    Create processing worker

---
+++ server/processing/worker.js
@@ -0,0 +1,17 @@
+const cron = require('node-cron');
+const Audio = require('../models/audio.js');
+const convertToText = require('./processing.js');
+
+module.exports = cron.schedule('0,5 * * * *', () => {
+
+  Audio.findNotProcessed()
+    .then(files => {
+      files.forEach(file => {
+        convertToText(file.audio_id, file.audio_path)
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    })
+}, false);
+ 
