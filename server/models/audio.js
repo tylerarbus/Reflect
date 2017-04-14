a2db10ffba38071a1b7cc63bcb0430d4b85066df
Scott <smclendening@gmail.com>
2017-04-13 21:32:58 -0700

    Add audio table methods

--- server/models/audio.js
+++ server/models/audio.js
@@ -9,3 +9,10 @@ module.exports.new = (audioPath) => {
     audioPath)
 }
 
+module.exports.update = (audio_id, column, updatedValue) => {
+  return db.query('UPDATE audio SET $1~ = $2 WHERE audio_id = $3 RETURNING *', [column, updatedValue, audio_id]);
+}
+
+module.exports.findNotProcessed = () => {
+  return db.oneOrNone('SELECT * FROM audio WHERE isProcessed = $1', [false]);
+}
