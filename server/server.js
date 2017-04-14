30334c92c09c20fcee01e4a9addadb45e811bbdf
Scott <smclendening@gmail.com>
2017-04-14 15:16:40 -0700

    Remove processingHandler from server

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const dev = require('./dev.js');
 const requestHandler = require('./requestHandler.js');
 const callingHandler = require('./calling/callingHandler.js');
 const authHandler = require('./auth/authHandler.js');
-const processingHandler = require('./processing/processing.js');
 const auth = require('./auth/utils.js');
 const Call = require('./calling/config.js');
 
