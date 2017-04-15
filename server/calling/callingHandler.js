85ac8477a335573fecc24850ad7410e5af2e3a05
Terence Tham <terence@epiphanyedu.com>
2017-04-15 16:22:47 -0700

    Integrate call-me-now button

--- server/calling/callingHandler.js
+++ server/calling/callingHandler.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const Call = require('./config.js');
 const xml = require('xml');
+const Auth = require('../auth/utils.js');
 
-router.post('/call', (req, res) => {
-	// TODO: Initiate call from Call.call(userId);
-	// Call.call();
-	res.status(200).send();
+router.post('/call', Auth.authMiddleware, (req, res) => {
+	Call.call(req.user)
+		.then(call_sid => {
+			res.status(200).send();
+		})
+		.catch(err => {
+			res.status(500).send('Error making call.');
+		});
 });
 
 router.post('/called', (req, res) => {
