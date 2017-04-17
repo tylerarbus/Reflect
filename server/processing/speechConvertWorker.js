b924ee30df6128d0349cc3041e13c6c0f90c059d
Terence Tham <terence@epiphanyedu.com>
2017-04-16 22:46:47 -0700

    Update speechConvertWorker with local_paths

--- server/processing/speechConvertWorker.js
+++ server/processing/speechConvertWorker.js
@@ -2,10 +2,11 @@ const cron = require('node-cron');
 const Audio = require('../models/audio.js');
 const convertToText = require('./processing.js');
 
-module.exports = cron.schedule('0,5 * * * *', () => {
-
+module.exports = cron.schedule('15 * * * * *', () => {
+	console.log('Speech Convert Worker: running...');
   Audio.findNotProcessed()
     .then(files => {
+    	console.log('SCW: Unprocessed audio files: ', files)
       files.forEach(file => {
         convertToText(file.audio_id, file.local_path, file.entry_id)
       });
