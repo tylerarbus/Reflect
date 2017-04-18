b55c2b5a668510ff42dc1fe109d595108bccfdf9
Terence Tham <terence@epiphanyedu.com>
2017-04-17 18:16:51 -0700

    Linted more server test files

--- test/server/auth/utils.test.js
+++ test/server/auth/utils.test.js
@@ -1,97 +1,86 @@
 require('dotenv').config();
 const auth = require('../../../server/auth/utils.js');
 const express = require('express');
-const request = require('supertest-as-promised');
+const request = require('supertest');
+
 let token;
 
-process.env.JWT_SECRET = 'secret';
-
 describe('Auth Utils tests', () => {
+  it('should have `hash, compare, sign and verify` functions', () => {
+    expect(auth.hash).toBeDefined();
+    expect(auth.compare).toBeDefined();
+    expect(auth.sign).toBeDefined();
+    expect(auth.verify).toBeDefined();
+  });
 
-	it('should have `hash, compare, sign and verify` functions', () => {
-		expect(auth.hash).toBeDefined();
-		expect(auth.compare).toBeDefined();
-		expect(auth.sign).toBeDefined();
-		expect(auth.verify).toBeDefined();
-	});
+  describe('Auth Hash utilities', () => {
+    const plainTextPassword = 'all your bases are belong to us';
+    const hashedPassword = '$2a$10$MkqMXMJAgsWxPY/qGTGYduuJnkqcA3g4A/x0FHaOtDNkUJ1dMgDp.';
+    it('`hash` should return promise with hash value', () => {
+      return auth.hash(plainTextPassword).then((val) => {
+        expect(val).toBeDefined();
+      });
+    });
 
-	describe('Auth Hash utilities', () => {
+    it('`compare` should return promise with boolean', () => {
+      return auth.compare(plainTextPassword, hashedPassword).then((val) => {
+        expect(typeof val).toBe('boolean');
+      });
+    });
+  });
 
-		const plainTextPassword = 'all your bases are belong to us';
-		const hashedPassword = '$2a$10$MkqMXMJAgsWxPY/qGTGYduuJnkqcA3g4A/x0FHaOtDNkUJ1dMgDp.';
-		
-		it('`hash` should return promise with hash value', () => {
-			return auth.hash(plainTextPassword).then(val => {
-				expect(val).toBeDefined();
-			});
-		});
+  describe('JWT utilities', () => {
+    const user = {
+      user_id: 1,
+      email: 'terence@email.com',
+      first_name: 'Terence',
+      last_name: 'Tham',
+      phone: '1234567834'
+    };
 
-		it('`compare` should return promise with boolean', () => {
-			return auth.compare(plainTextPassword, hashedPassword).then(val => {
-				expect(typeof val).toBe('boolean');
-			});
-		});
+    token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlcmVuY2VAZW1haWwuY29tIiwiaWF0IjoxNDkxOTQzODU3LCJleHAiOjE0OTIxMTY2NTd9.hKSNpf7n65iFpCOahyK8tX9W-IP3-Ng9lY_B2DesPWw';
 
-	});
+    it('`sign` should return a promise', () => {
+      return auth.sign(user).then((val) => {
+        expect(val).toBeDefined();
+        token = val;
+      });
+    });
 
-	describe('JWT utilities', () => {
+    it('`verify` should return a promise', () => {
+      return auth.verify(token).then((result) => {
+        expect(result.email).toEqual(user.email);
+      });
+    });
+  });
 
-		let user = {
-			user_id: 1,
-			email: 'terence@email.com',
-			first_name: 'Terence',
-			last_name: 'Tham',
-			phone: '1234567834'
-		}
+  describe('Auth Middleware test', () => {
+    it('sets req.user with user if successful.', () => {  
+      const app = express();
+      token = `Bearer ${token}`;
 
-		token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlcmVuY2VAZW1haWwuY29tIiwiaWF0IjoxNDkxOTQzODU3LCJleHAiOjE0OTIxMTY2NTd9.hKSNpf7n65iFpCOahyK8tX9W-IP3-Ng9lY_B2DesPWw';
+      app.use(auth.authMiddleware, (req, res, next) => {
+        expect(req.user).toBeDefined();
+        res.end();
+      });
 
-		it('`sign` should return a promise', () => {
-			return auth.sign(user).then(val => {
-				expect(val).toBeDefined();
-				token = val;
-			})
-		});
+      return request(app)
+        .get('/')
+        .set({ authorization: token })
+        .expect(200);
+    });
 
-		it('`verify` should return a promise', () => {
-			return auth.verify(token).then(result => {
-				expect(result.email).toEqual(user.email);
-			})
-		});
+    it('returns an error if no token is set', () => {
+      const app = express();
+      token = 'Bearer this is an invalid token';
 
-	});
-
-	describe('Auth Middleware test', () => {
-		
-		it('sets req.user with user if successful.', () => {	
-			let app = express();
-			token = 'Bearer ' + token;
-
-			app.use(auth.authMiddleware, (req, res, next) => {
-				expect(req.user).toBeDefined();
-				res.end();
-			});
-
-			return request(app)
-				.get('/')
-				.set({ 'authorization': token })
-				.expect(200)
-		});
-
-		it('returns an error if no token is set', () => {
-			let app = express();
-			let token = 'Bearer this is an invalid token';
-
-			app.use(auth.authMiddleware, (req, res, next) => {
-				res.end();
-			});
-			
-			return request(app)
-				.get('/')
-				.set({ 'authorization': token })
-				.expect(401)
-		});
-	});
-
+      app.use(auth.authMiddleware, (req, res, next) => {
+        res.end();
+      });
+      return request(app)
+        .get('/')
+        .set({ authorization: token })
+        .expect(401);
+    });
+  });
 });
-
