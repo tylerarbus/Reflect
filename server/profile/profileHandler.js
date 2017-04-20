a773f201938310ae7b2490c7a81b2e9548e97240
Jason Chang <jay3c14@gmail.com>
2017-04-20 15:18:43 -0700

    Create profileHandler router

--- server/profile/profileHandler.js
+++ server/profile/profileHandler.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const Prefs = require('../models/call-preferences.js');
 
 router.post('/callpreferences', (req, res) => {
-
+  const { userId, timeOfDay } = req.body;
+  Prefs.new(userId, timeOfDay)
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch((err) => {
+      console.log('Error: ', err);
+      res.status(400).json({
+        error: 'Error adding phone prefs.'
+      });
+    });
 });
 
 module.exports = router;
