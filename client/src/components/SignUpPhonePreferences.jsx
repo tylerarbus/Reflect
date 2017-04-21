61c085c9dca64c95b8833ba973dcb8a58923fca0
Jason Chang <jay3c14@gmail.com>
2017-04-21 09:58:00 -0700

    Edit minute selector to only display 15min intervals

--- client/src/components/SignUpPhonePreferences.jsx
+++ client/src/components/SignUpPhonePreferences.jsx
@@ -92,17 +92,9 @@ export class SignUpPhonePreferences extends Component {
                 >
                   <option value="">Minute</option>
                   <option value="00">00</option>
-                  <option value="05">05</option>
-                  <option value="10">10</option>
                   <option value="15">15</option>
-                  <option value="20">20</option>
-                  <option value="25">25</option>
                   <option value="30">30</option>
-                  <option value="35">35</option>
-                  <option value="40">40</option>
                   <option value="45">45</option>
-                  <option value="50">50</option>
-                  <option value="55">55</option>
                 </select>
               </div>
               <div className="two wide field">
