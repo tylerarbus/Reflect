2efc3aef83e2a52c286af0ec7189837605df4a98
Terence Tham <terence@epiphanyedu.com>
2017-04-22 17:09:34 -0700

    Refactor downloadWorker.getFileDetails into its own worker

--- server/processing/downloadWorker.js
+++ server/processing/downloadWorker.js
@@ -1,5 +1,4 @@
 const cron = require('node-cron');
-const Call = require('../calling/config.js');
 const Audio = require('../models/audio.js');
 const Entry = require('../models/entries.js');
 const request = require('request');
@@ -20,52 +19,7 @@ const _downloadFile = (recordingId, callId, entryId) => {
     ));
 };
 
-module.exports.getFileDetails = cron.schedule('5 * * * * *', () => {
-  console.log('DownloadWorker: getFileDetails running...');
-
-  Entry.findNotProcessed()
-    .then((results) => {
-      if (!results) {
-        throw new Error('No unprocessed Entries.');
-      }
-      return Call.getRecordings();
-    })
-    .then((calls) => {
-      if (calls.recordings.length === 0) {
-        throw new Error('DW: Get file: no recordings found on server.');
-      }
-      calls.recordings.forEach(recording => (
-        Entry.getByCallId(recording.call_sid)
-          .then((entry) => {
-            if (!entry) {
-              throw new Error('No entry found.');
-            }
-            return Audio.new({
-              entry_id: entry.entry_id,
-              remote_path: recording.uri,
-              local_path: '',
-              is_processed: false,
-              is_downloaded: false,
-              recording_id: recording.sid,
-              date_file_created: recording.dateCreated
-            });
-          })
-          .then((audio) => {
-            if (audio) {
-              return Entry.updateProcessed(audio.entry_id);
-            }
-          })
-          .catch((err) => {
-            console.log(err);
-          })
-      ));
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}, false);
-
-module.exports.downloadFiles = cron.schedule('10 * * * * *', () => {
+module.exports = cron.schedule('10 * * * * *', () => {
   console.log('DownloadWorker: downloadFiles running...');
   Audio.findNotDownloaded()
     .then((results) => {
