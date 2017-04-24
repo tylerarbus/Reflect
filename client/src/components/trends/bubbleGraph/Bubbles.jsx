3862e592fc23b1a069bac929f63cf2fed184da38
Scott <smclendening@gmail.com>
2017-04-23 22:05:24 -0700

    Refactor to allow for reusability

--- client/src/components/trends/bubbleGraph/Bubbles.jsx
+++ client/src/components/trends/bubbleGraph/Bubbles.jsx
@@ -7,68 +7,68 @@ export class Bubbles extends Component {
     super(props);
     this.state = { g: null };
 
-   // this.renderBubbles = this.renderBubbles.bind(this);
+    this.renderBubbles = this.renderBubbles.bind(this);
+
+    this.simulation = d3.forceSimulation()
+     .force("x", d3.forceX().strength(0.03).x(this.props.width / 2))
+     .force("y", d3.forceY().strength(0.03).y(this.props.height / 2))
+     .on('tick', this.ticked)
+     .force('charge', d3.forceManyBody().strength(-40))
+     .stop();
   }
 
   componentDidMount() {
-    const { width, height, margin, keywordData } = this.props;
-    const forceStrength = 0.03
-   // this.renderBubbles(dummyKeywords);
-    const bubbles = d3.select('.bubbles').selectAll('circle')   
-      .data(keywordData)
+    const { keywordData } = this.props;
+    this.renderBubbles(keywordData);
+  }
 
-    const bubblesE = bubbles
-      .enter()
-      .append('circle')
+  ticked() {
+    const bubbles = d3.select('.bubbleChartContainer').selectAll('circle');
+    const text = d3.select('.bubbleChartContainer').selectAll('text');
+
+    bubbles
+        .attr('cx', d => d.x)
+        .attr('cy', d => d.y);
+
+    text
+        .attr('x', d => d.x)
+        .attr('y', d => d.y);
+  }
+
+  renderBubbles(data) {
+    const { width, height, margin } = this.props;
+    const forceStrength = 0.03;
+    const bubbles = d3.select('.bubbleChartContainer').selectAll('circle')
+      .data(data);
+
+    bubbles.exit().remove()
+
+    const bubblesE = bubbles.enter().append('circle')
+        .classed('bubble', true)
         .attr('r', d => d.r)//d.r)
         // .attr('cx', d => d.x + 50 - margin.right)
         // .attr('cy', d => d.y + 50 - margin.top)
         .attr('fill', '#89EEB2');
 
-    const text = d3.select('.bubbles').selectAll('text')
-      .data(keywordData)
-      .enter()
-      .append('text')
+    const text = d3.select('.bubbleChartContainer').selectAll('text')
+      .data(data).enter().append('text')
         // .attr('x', d => d.x + 50 - margin.right)
         // .attr('y', d => d.y + 50 - margin.top)
         .text(d => d.word)
-        .attr('font-family', 'sans-serif')
+        .attr('font-family', 'helvetica')
         .attr('font-size', '10px')
         .attr('text-anchor', 'middle')
         .attr('fill', 'black');
-
-    const simulation = d3.forceSimulation()
-     .force("x", d3.forceX().strength(forceStrength).x(width / 2))
-     .force("y", d3.forceY().strength(forceStrength).y(height / 2))
-     .on('tick', ticked)
-     .force('charge', d3.forceManyBody().strength(-25));
-     //.nodes(keywordData)
-     //.force("collide", d3.forceCollide(30));
-
-    function ticked (e) {
-      console.log('tick')
-      bubblesE
-          .attr('cx', d => d.x)
-          .attr('cy', d => d.y);
-
-      text
-          .attr('x', d => d.x)
-          .attr('y', d => d.y);
-    }
-
-    function charge(d) {
-  return -forceStrength * Math.pow(d.r, 2.0);
-}
   
-  simulation
-    .nodes(keywordData)
+    this.simulation
+      .nodes(data).alpha(1).restart()
 
   }
 
 
   render() {
     return (
-      <g className="bubbles" />
+      <g className="bubble" />
     )
   }
 }
