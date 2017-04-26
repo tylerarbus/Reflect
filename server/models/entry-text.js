846c50a80b41bd8e50a488bcab7f7a5ae92b0ce1
Tyler Arbus <tyler.arbus@gmail.com>
2017-04-25 22:36:27 -0700

    Configures search for deployment; refactor to improve query structure

--- server/models/entry-text.js
+++ server/models/entry-text.js
@@ -14,11 +14,15 @@ module.exports.findNotAnalyzed = () => (
 );
 
 module.exports.findNotIndexed = () => (
-  db.any('SELECT entry_text.entry_id, entry_text.entry_text_id, entry_text.text, entries.created, entries.user_id, audio.local_path FROM entry_text\
+  db.any('SELECT entry_text.entry_id, entry_text.text, entries.created, entries.user_id, audio.local_path FROM entry_text\
   INNER JOIN entries ON entry_text.is_indexed = false AND entries.entry_id = entry_text.entry_id\
   INNER JOIN audio ON entries.entry_id = audio.entry_id')
 );
 
 module.exports.update = (entryTextId, column, updatedValue) => (
   db.query('UPDATE entry_text SET $1~ = $2 WHERE entry_text_id = $3 RETURNING *', [column, updatedValue, entryTextId])
+);
+
+module.exports.updateByEntryId = (entryId, column, updatedValue) => (
+  db.query('UPDATE entry_text SET $1~ = $2 WHERE entry_id = $3 RETURNING *', [column, updatedValue, entryId])
 );
